9b9a42e4a06c7efcd8b908a582cbecb9705eaf3e
kindaserene <thanhbinhvu.q@gmail.com>
2025-07-27 09:44:11 +0800

    change from console log to UI interaction

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-let RPS = ["rock", "paper", "scissor"];
-humanScore = 0;
-computerScore = 0;
+let RPS = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
+let currentRound = 0;
+
+const roundNo = document.getElementById("roundNo");
+const humanScoreDiv = document.getElementById("humanScore");
+const compScoreDiv = document.getElementById("compScore");
+const final = document.getElementById("final");
+
+const rBtn = document.getElementById("rock");
+const pBtn = document.getElementById("paper");
+const sBtn = document.getElementById("scissors");
+const resetBtn = document.getElementById("reset");
+
+rBtn.addEventListener("click", () => playRound("rock"));
+pBtn.addEventListener("click", () => playRound("paper"));
+sBtn.addEventListener("click", () => playRound("scissors"));
+
 
 function getComputerChoice() {
     let idx = Math.floor(Math.random() * 3);
@@ -8,52 +24,54 @@ function getComputerChoice() {
     return compChoice;
 }
 
-function getHumanChoice() {
-    let humanInput = prompt("Rock, Paper or Scissor?");
-    return humanInput.toLowerCase();
-}
-
-function playRound() {
-    let h = getHumanChoice();
+function playRound(humanChoice) {
     let c = getComputerChoice();
+    let h = humanChoice;
+    currentRound++;
 
-    if (h === c) {
-        console.log("It's a tie. Let's play again");
-    } else if (h==="rock" &&  c==="paper")  {
+    if (c === h) {
+    } else if (c === "rock" && h === "scissors" ||
+        c === "paper" && h === "rock" ||
+        c === "scissors" && h === "paper") {                
         humanScore++;
-        console.log("You lose! Rock beats Paper");
-    } else if (h==="rock" &&  c==="scissor")  {
-        humanScore++;
-        console.log("You win! Rock beats Scissor");
-    } else if (h==="paper" &&  c==="rock")  {      
-        humanScore++;
-        console.log("You win! Paper beats Rock");    
-    } else if (h==="paper" &&  c==="scissor")  {
-        computerScore++;
-        console.log("You lose! Paper beats Scissor");
-    } else if (h==="scissor" &&  c==="rock")  {
-        computerScore++;
-        console.log("You lose! Scissor beats Rock");
-    } else if (h==="scissor" &&  c==="paper")  {
-        humanScore++;
-        console.log("You win! Scissor beats Paper");
-    }
-}
-
-function playGame () {
-    for (let i = 1; i <= 5; i++) {
-        console.log(`\n--- Round ${i} ---`);
-        playRound();
-        console.log(`Score: You ${humanScore} - ${computerScore} Computer`);    
-    }
-
-    if (humanScore > computerScore) {
-        console.log("You win the game!");
-    } else if (computerScore < humanScore) {
-        console.log("You lose the game...");
     } else {
-        console.log("It's a tie. Wanna play again?");
+        computerScore++;
     }
+    
+    roundNo.textContent = "Round: " + currentRound;
+    humanScoreDiv.textContent = "Human Score: " + humanScore;
+    compScoreDiv.textContent = "Computer Score: " + computerScore;
+    
+    if (humanScore === 5) {
+        final.textContent = "Human Wins! 💃";
+        resetBtn.style.display = "block";
+        rBtn.disabled = true;
+        pBtn.disabled = true;
+        sBtn.disabled = true;
+    } else if (computerScore === 5) {
+        final.textContent = "Computer Wins! 🤖";
+        resetBtn.style.display = "block";
+        rBtn.disabled = true;
+        pBtn.disabled = true;
+        sBtn.disabled = true;        
+    }
+
 }
 
-playGame()
+    resetBtn.addEventListener("click", function() {
+        humanScore = 0;
+        computerScore = 0;
+        currentRound = 0;
+
+        humanScoreDiv.textContent = "Human Score: 0";
+        compScoreDiv.textContent = "Computer Score: 0";
+        roundNo.textContent = "Round: 0";
+        final.textContent = "Final Winner:";
+
+        sBtn.disabled = false;
+        pBtn.disabled = false;    
+        rBtn.disabled = false;
+
+        resetBtn.style.display = "none";
+    }
+    )
